f6270350726a39fc59eedd50407c5d13817b6cff
Hiago Souza <hiagofss98@gmail.com>
2021-01-22 20:42:15 -0300

    React Hooks tests

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -89,7 +89,7 @@ function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must be used within an AuphProbider');
+    throw new Error('useAuth must be used within an AuthProvider');
   }
 
   return context;
